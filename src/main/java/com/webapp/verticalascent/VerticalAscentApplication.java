919ec760964e7f59fc40e875a319afcf1f3e096b
nathan letournel <letournel.nathan@gmail.com>
2023-09-12 18:56:44 +0200

    Config env local and remote

--- src/main/java/com/webapp/verticalascent/VerticalAscentApplication.java
+++ src/main/java/com/webapp/verticalascent/VerticalAscentApplication.java
@@ -2,12 +2,15 @@ package com.webapp.verticalascent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class VerticalAscentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VerticalAscentApplication.class, args);
+		SpringApplication app = new SpringApplication(VerticalAscentApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		int port = Integer.parseInt(env.getProperty("PORT", "420"));
+		System.setProperty("server.port", String.valueOf(port));
 	}
-
 }
