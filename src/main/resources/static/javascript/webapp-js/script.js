2f6d354b7a66a6264a4ade905db82d9ddcca809c
nathan letournel <letournel.nathan@gmail.com>
2023-09-27 09:33:44 +0200

    Update: fixing esling config

---
+++ src/main/resources/static/javascript/webapp-js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Testing Eslint
+ *
+ * @param {number} a - This is a description.
+ * @param {number} b - This is a description.
+ * @return {boolean} This is a description.
+ */
+function esLinttes(a, b) {
+	return a === b;
+}
+	esLinttes(1, 1);
