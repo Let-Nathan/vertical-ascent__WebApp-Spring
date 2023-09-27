92a90bc5726b9fd38ffb7348891dfab31af3d0a1
nathan letournel <letournel.nathan@gmail.com>
2023-09-27 23:56:15 +0200

    Fix: Github action eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": ['eslint:recommended', 'google'],
+    "extends": ["eslint:recommended", "google"],
     "overrides": [
         {
             "env": {
@@ -21,5 +21,6 @@ module.exports = {
         ecmaVersion: 12,
     },
     "rules": {
+        "indent" : "off"
     }
 }
