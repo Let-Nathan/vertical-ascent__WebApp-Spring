c6eb0f1980aad55f4b996e5e035f090953a313fb
Nathan Letournel <99188075+Let-Nathan@users.noreply.github.com>
2023-09-28 00:50:53 +0200

    Fixing history (#6)
    * Test: Check if checkstyle action working
    
    * Fix: Github action job on pull request
    
    * Inprogres: working nav_bar
    
    * Create: Add Stylelint & HtmlHint --> Checkout html / css files
    
    * Saved dev changes
    
    * Test: Check html & css verify
    
    * Fix: Limit Lintcss exec on style.css
    
    * Fix: Limit Lintcss exec on style.css
    
    * Fix: Limit Lintcss exec on style.css
    
    * Update: Config to verify js files
    
    * Fix: Config to verify js files
    
    * Fix: Config to verify js files
    
    * Fix: Config to verify js files
    
    * Fix: Config to verify js files
    
    * Fix: Config to verify js files
    
    * Fix: Config to verify js files
    
    * Add: Add local StyleLint
    
    * Add: Add local StyleLint
    
    * Add: Add local StyleLint
    
    * Add: Add local StyleLint
    
    * Fix: Config build
    
    * RollBack: Yesterday config
    
    * RollBack: Yesterday config
    
    * Update: Modify build yml --> split CI
    
    * Dev pull
    
    * Update: Ci with github action eslint check, create package.json
    
    * Update: ESLint Path add /static/
    
    * Update: ESLint Path add /static/
    
    * Update: fixing esling config
    
    * Fix: Remove indentation conf, broken wont be patch by author
    
    * Fix: Github action eslint
    
    * Fix: Github action eslint
    
    * Fix: Github action eslint
    
    * Fix: Eslint & github action --> working
    
    * dev
    
    * before fusion
    
    * Delete: Node module
    
    * Fix: history
    
    * Fix: history
    
    * Fix: history
    
    * Delete: Gradle files

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": ["eslint:recommended", "google"],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        ecmaVersion: 12,
+    },
+    "rules": {
+        "indent" : "off"
+    }
+}
