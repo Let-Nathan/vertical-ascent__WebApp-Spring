00ccc0658f273aea44e47ba37aad6adad282cf66
nathan letournel <letournel.nathan@gmail.com>
2023-10-05 12:42:52 +0200

    Create: Container with img + category name

--- src/main/resources/static/javascript/webapp-js/navbar_script.js
+++ src/main/resources/static/javascript/webapp-js/navbar_script.js
@@ -3,14 +3,12 @@ fetch('api/product-categories')
         if (!response.ok) {
             throw new Error('Unable to obtain response from API' + err);
         }
-        console.log('Response ==>' + response);
         return response.json();
     })
     .then(data => {
         data.forEach(item => {
            menuContainer(item);
         })
-        console.log('ok');
     })
     .catch(err => {
         console.error('Error while get data ==>' + err);
@@ -24,15 +22,32 @@ function menuContainer(item) {
     const divContainer = document.createElement('div');
     const aElement = document.createElement('a');
     const imgBackGround = document.createElement('img');
+    const spanCategoryTitle = document.createElement('span');
 
-    listElement.className = "category " + item.name;
-    divContainer.className = "container-fluid d-flex flex-column align-items-start";
+    listElement.className = "category " + item.name + " nav-menu-list";
 
-    aElement.id = item.id;
     aElement.href = "product-category/" + item.id;
+    aElement.class = "aElement";
+    aElement.style.textDecoration = "none";
 
-    divContainer.append(aElement);
-    listElement.append(divContainer);
+    divContainer.className = "nav-menu-container shadow-sm p-2 mb-2 container-fluid";
+    //Todo replace image name by item.name
+    divContainer.style.backgroundImage = 'url("../images/navbar_category/randonnee-bivouac.jpg")';
+    divContainer.style.backgroundRepeat = "no-repeat";
+    divContainer.style.backgroundPosition = "center";
+    divContainer.style.boxShadow = "-10px 0px 10px -5px rgba(0,0,0,0.3)";
+
+    // divContainer.style.objectFit = "cover";
+
+
+    spanCategoryTitle.textContent = item.name;
+    spanCategoryTitle.className = "fs-6 fw-bold text-light text-uppercase col-4";
+
+    divContainer.append(spanCategoryTitle);
+    // divContainer.append(imgBackGround);
+    aElement.append(divContainer);
+    listElement.append(aElement);
     ulSelector.append(listElement);
 
 }
+
