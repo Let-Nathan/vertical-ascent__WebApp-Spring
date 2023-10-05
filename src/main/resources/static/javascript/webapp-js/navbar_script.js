4b076414c40c6f1fdbcbc7bbf2d35f36b01941e6
nathan letournel <letournel.nathan@gmail.com>
2023-10-05 12:49:52 +0200

    Refactoring: Delete useless code / Add comments

--- src/main/resources/static/javascript/webapp-js/navbar_script.js
+++ src/main/resources/static/javascript/webapp-js/navbar_script.js
@@ -1,3 +1,6 @@
+/**
+ * Fetch api product categories to create category navbar menu
+ */
 fetch('api/product-categories')
     .then(response => {
         if (!response.ok) {
@@ -17,37 +20,43 @@ fetch('api/product-categories')
 
 function menuContainer(item) {
 
+    // Init whole list / container for menu //
     const ulSelector = document.querySelector('.menu-categories');
     const listElement = document.createElement('li');
     const divContainer = document.createElement('div');
     const aElement = document.createElement('a');
-    const imgBackGround = document.createElement('img');
     const spanCategoryTitle = document.createElement('span');
+    // -------------------------------- //
 
+    // <list> element //
     listElement.className = "category " + item.name + " nav-menu-list";
+    // -------------------------------- //
 
+    // <a> element make container clickable //
     aElement.href = "product-category/" + item.id;
     aElement.class = "aElement";
     aElement.style.textDecoration = "none";
+    // -------------------------------- //
 
+    // <div> element with background image //
     divContainer.className = "nav-menu-container shadow-sm p-2 mb-2 container-fluid";
     //Todo replace image name by item.name
     divContainer.style.backgroundImage = 'url("../images/navbar_category/randonnee-bivouac.jpg")';
     divContainer.style.backgroundRepeat = "no-repeat";
     divContainer.style.backgroundPosition = "center";
     divContainer.style.boxShadow = "-10px 0px 10px -5px rgba(0,0,0,0.3)";
+    // -------------------------------- //
 
-    // divContainer.style.objectFit = "cover";
-
-
+    // <span> element with category name
     spanCategoryTitle.textContent = item.name;
     spanCategoryTitle.className = "fs-6 fw-bold text-light text-uppercase col-4";
+    // -------------------------------- //
 
+    // Assembly of elements  //
     divContainer.append(spanCategoryTitle);
-    // divContainer.append(imgBackGround);
     aElement.append(divContainer);
     listElement.append(aElement);
     ulSelector.append(listElement);
-
+    // -------------------------------- //
 }
 
