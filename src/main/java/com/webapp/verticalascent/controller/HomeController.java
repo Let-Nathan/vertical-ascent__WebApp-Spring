7e37e0d079ccf8f82b8c4095730ac8e1e89dcbc6
nathan letournel <letournel.nathan@gmail.com>
2023-10-14 16:23:00 +0200

    Fix: lintcss and checkstyle errors

--- src/main/java/com/webapp/verticalascent/controller/HomeController.java
+++ src/main/java/com/webapp/verticalascent/controller/HomeController.java
@@ -5,9 +5,8 @@ import com.webapp.verticalascent.service.ProductCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Controller class for Home path.
