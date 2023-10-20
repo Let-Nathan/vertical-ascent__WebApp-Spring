dd1ff178001dea2ccd60c28c85571e093c64cb22
nathan letournel <letournel.nathan@gmail.com>
2023-10-20 03:05:08 +0200

    Create(design/controller): Implement controller / Implement view

---
+++ src/main/java/com/webapp/verticalascent/controller/ProductCategoryRestController.java
@@ -0,0 +1,34 @@
+package com.webapp.verticalascent.controller;
+
+import com.webapp.verticalascent.entity.ProductCategory;
+import com.webapp.verticalascent.service.ProductCategoryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * API REST Product Categories.
+ *
+ * @author Nathan L
+ * @version 1.0
+ *
+ */
+@RestController
+@RequestMapping("/api/product-categories")
+public class ProductCategoryRestController {
+ 
+	private final ProductCategoryService productCategoryService;
+	
+    @Autowired
+    public ProductCategoryRestController(ProductCategoryService productCategoryService) {
+	    this.productCategoryService = productCategoryService;
+	}
+	
+    @GetMapping
+    public List<ProductCategory> getAllCategories() {
+	    return productCategoryService.getAllProductCategory();
+	}
+	
+}
