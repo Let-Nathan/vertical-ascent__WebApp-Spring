d8063553f3d1d21543fe5d85f46c9dcbda966c81
nathan letournel <letournel.nathan@gmail.com>
2023-10-22 16:50:24 +0200

    Resolve CI errors

--- src/main/resources/static/javascript/webapp-js/productMobileFilter.js
+++ src/main/resources/static/javascript/webapp-js/productMobileFilter.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const MOBILE_FILTER_TRIGGER = document.getElementById('mobile-filter-container');
-    const DESKTOP_FILTER_CONTAINER = document.getElementById('filter-desktop-container');
+    const MOBILE_FILTER_TRIGGER =
+        document.getElementById('mobile-filter-container');
+    const DESKTOP_FILTER_CONTAINER =
+        document.getElementById('filter-desktop-container');
 
-    MOBILE_FILTER_TRIGGER.addEventListener('click', function() {
-        // Ajoutez ou supprimez la classe 'd-none' pour afficher ou masquer le conteneur de filtrage de bureau
+    MOBILE_FILTER_TRIGGER.addEventListener('click', function()
+    {
         DESKTOP_FILTER_CONTAINER.classList.toggle('d-none');
     });
 });
