054017bd9e67ddad559ac47db548bb6bca420e13
nathan letournel <letournel.nathan@gmail.com>
2023-10-23 02:47:19 +0200

    Update(shopping-step1): Pages mobile compliance, lf ==> Desktop / Section product suggestion

--- src/main/java/com/webapp/verticalascent/controller/ShoppingProcessController.java
+++ src/main/java/com/webapp/verticalascent/controller/ShoppingProcessController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  *
  */
 @Controller
-public class PurchasingProcessController {
+public class ShoppingProcessController {
 	
 	@GetMapping("/shopping_cart")
 	public final String shoppingCart() {
