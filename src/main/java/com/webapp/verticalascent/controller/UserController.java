734f26859bea18dcfe5c616533faf2e32b1cacc8
nathan letournel <letournel.nathan@gmail.com>
2023-10-29 13:01:11 +0100

    Create(vc): view & controller for user account page

---
+++ src/main/java/com/webapp/verticalascent/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.webapp.verticalascent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller class for User.
+ *
+ * @author Nathan L.
+ * @version 1.0
+ *
+ */
+@Controller
+public class UserController {
+	
+	@GetMapping("/account")
+	public final String login() {
+		return "user-account";
+	}
+	
+}
