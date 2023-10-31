536950ecbfae7e33a1e56d16d4fa91cd6529a725
nathan letournel <letournel.nathan@gmail.com>
2023-10-31 01:04:54 +0100

    Update(DTO): Trying to solve DTO form validation

--- src/main/java/com/webapp/verticalascent/dto/UserRegistrationDTO.java
+++ src/main/java/com/webapp/verticalascent/dto/UserRegistrationDTO.java
@@ -1,11 +1,8 @@
 package com.webapp.verticalascent.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
-
-import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -16,11 +13,30 @@ import java.util.Date;
  */
 @Data
 public class UserRegistrationDTO {
+	
+	@NotBlank(message = "Merci de renseigner un prénom")
+	@NotEmpty
 	private String firstName;
+	
+	@NotBlank(message = "Merci de renseigner votre nom de famille")
+	@NotEmpty
 	private String lastName;
+	
+	@NotBlank
+	@NotEmpty
+	@Pattern(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "L'adresse e-mail n'est pas valide")
 	private String email;
+	
+	@NotBlank
+	@NotEmpty
+	@Size(min = 8, max = 32, message = "Le mot de passe doit contenir entre 8 et 32 caracthères")
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Le mot de passe doit contenir au minimum 8 caracthères, un chiffre, une lettre minuscule et une majuscule et un caracthère spécial")
 	private String password;
+	
+//	@Pattern(regexp = "^(?:\\+\\d+|\\d+)$", message = "Le champ doit contenir uniquement des chiffres.")
+	@Size(min = 10, message = "Vous devez renseigner un nuémro valide, avec 10 chiffres")
 	private String mobilePhone;
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date birthDate;
 	
