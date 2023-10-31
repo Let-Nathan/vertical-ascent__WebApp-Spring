16bf8d5cab9f4c0378d0e10c40a2c314bb3eef4a
nathan letournel <letournel.nathan@gmail.com>
2023-10-31 11:14:34 +0100

    Fix(Form): DTO validation with errors diplay on view

--- src/main/java/com/webapp/verticalascent/controller/RegistrationController.java
+++ src/main/java/com/webapp/verticalascent/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -51,7 +52,7 @@ public class RegistrationController {
 	}
 	
 	@RequestMapping(value = "/register/save")
-	public String registerUser (
+	public String registerSaved (
 		@Valid UserRegistrationDTO userRegistrationDTO,
 		BindingResult result
 	) {
