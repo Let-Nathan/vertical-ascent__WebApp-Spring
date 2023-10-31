beb4f0a0cd4b067af13c0c145ec0cebf233873d6
nathan letournel <letournel.nathan@gmail.com>
2023-10-31 15:04:03 +0100

    Fix(validation): Form need to be valide before submition

--- src/main/java/com/webapp/verticalascent/dto/UserRegistrationDTO.java
+++ src/main/java/com/webapp/verticalascent/dto/UserRegistrationDTO.java
@@ -2,9 +2,6 @@ package com.webapp.verticalascent.dto;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.util.Date;
 
@@ -17,31 +14,31 @@ import java.util.Date;
 @Data
 public class UserRegistrationDTO {
 	
-	@NotBlank(message = "Merci de renseigner un prénom")
+	@NotBlank(message = "Merci de renseigner un prénom.")
 	@NotEmpty
 	private String firstName;
 	
-	@NotBlank(message = "Merci de renseigner votre nom de famille")
+	@NotBlank(message = "Merci de renseigner votre nom de famille.")
 	@NotEmpty
-	@Size(min = 2, max = 3)
 	private String lastName;
 	
 	@NotBlank
 	@NotEmpty
-	@Pattern(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "L'adresse e-mail n'est pas valide")
+	@Pattern(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "L'adresse e-mail n'est pas valide.")
 	private String email;
 	
 	@NotBlank
 	@NotEmpty
-	@Size(min = 8, max = 32, message = "Le mot de passe doit contenir entre 8 et 32 caracthères")
-	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Le mot de passe doit contenir au minimum 8 caracthères, un chiffre, une lettre minuscule et une majuscule et un caracthère spécial")
+	@Size(min = 8, max = 32, message = "Le mot de passe doit contenir entre 8 et 32 caracthères.")
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Le mot de passe doit contenir, un chiffre, une lettre minuscule et une majuscule et un caracthère spécial.")
 	private String password;
 	
-//	@Pattern(regexp = "^(?:\\+\\d+|\\d+)$", message = "Le champ doit contenir uniquement des chiffres.")
-	@Size(min = 10, message = "Vous devez renseigner un nuémro valide, avec 10 chiffres")
+	@Pattern(regexp = "^(?:\\+\\d+|\\d+)$", message = "Le téléphone doit être au format chiffre ou avec +33.")
+	@Size(min = 10, message = "Vous devez renseigner un nuémro valide, avec 10 chiffres.")
 	private String mobilePhone;
 	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Past(message = "La date ne peut pas être dans le futur.")
 	private Date birthDate;
 	
 }
