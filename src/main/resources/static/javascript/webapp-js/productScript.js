a8af190c7ac81c4dd6514876b72c634bc5035243
nathan letournel <letournel.nathan@gmail.com>
2023-12-08 13:24:32 +0100

    Fix(error): With in regards of Eslint

--- src/main/resources/static/javascript/webapp-js/productScript.js
+++ src/main/resources/static/javascript/webapp-js/productScript.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Get the add product button.
+    // Get the add product button.
     const ADD_PRODUCT = document.getElementById('add-product');
-    //Bootstrap modal initialization.
-    const MODAL = new bootstrap.Modal(document.getElementById('addToCartModal'), {
+    // Bootstrap modal initialization.
+    const MODAL = new bootstrap.Modal(
+        document.getElementById('addToCartModal'), {
         keyboard: false
     });
     const ITEM_INCREMENT = document.getElementById('item-plus');
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         quantity++;
         CART_QUANTITY.innerText = quantity;
 
-        updateLocalStorage(quantity);
+        updateLocalStorageQuantity(quantity);
     });
 
     ITEM_DECREMENT.addEventListener('click', function() {
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
             quantity--;
             CART_QUANTITY.innerText = quantity;
 
-            updateLocalStorage(quantity);
+            updateLocalStorageQuantity(quantity);
         }
     });
 
@@ -38,39 +39,44 @@ document.addEventListener('DOMContentLoaded', function() {
         const productPrice = ADD_PRODUCT.getAttribute('data-product-price');
         const productQuantity = ADD_PRODUCT.getAttribute('data-product-quantity');
 
-        //Simple user verification, we will re check data before create
+        // Simple user verification, we will re check data before create
         // cart user and shopping session.
         if (productQuantity === '0') {
             alert('Product not in stock');
             return;
         }
 
-        let cart = JSON.parse(localStorage.getItem('userCart')) || [];
-        let productInCart = cart.find(item => item.productId === productId);
+        const CART = JSON.parse(localStorage.getItem('userCart')) || [];
+        const PRODUCTINCART = CART.find((item) => item.productId === productId);
         let quantity = 1;
 
-        if (productInCart) {
-            //If product is already in local storage we increment quantity by 1.
-            productInCart.quantity += 1;
-            quantity = productInCart.quantity;
+        if (PRODUCTINCART) {
+            // If product is already in local storage we increment quantity by 1.
+            PRODUCTINCART.quantity += 1;
+            quantity = PRODUCTINCART.quantity;
         } else {
-            //If no product, we initialized one with a default quantity.
-            cart.push({
+            // If no product, we initialized one with a default quantity.
+            CART.push({
                 productId: productId,
                 productName: productName,
                 productPrice: productPrice,
                 quantity: 1
             });
         }
-        //Update the local storage.
-        localStorage.setItem('userCart', JSON.stringify(cart));
+        // Update the local storage.
+        localStorage.setItem('userCart', JSON.stringify(CART));
 
-        //Display modal with updated quantity
+        // Display modal with updated quantity.
         document.getElementById('cartQuantity').innerText = quantity;
         MODAL.show();
     });
 
-    function updateLocalStorage(quantity) {
+    /**
+     * Upate the product quantity in local storage.
+     *
+     * @param quantity
+     */
+    function updateLocalStorageQuantity(quantity) {
         const productId = ADD_PRODUCT.getAttribute('data-product-id');
         let cart = JSON.parse(localStorage.getItem('userCart')) || [];
         let productInCart = cart.find(item => item.productId === productId);
