bf9adb730356b4b527939b1b14bd57efd2602cc2
nathan letournel <letournel.nathan@gmail.com>
2023-12-17 19:43:25 +0100

    Fix last bugs

--- src/main/resources/static/javascript/webapp-js/registerFormScript.js
+++ src/main/resources/static/javascript/webapp-js/registerFormScript.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('votre-formulaire-id');
+    const form = document.getElementById('register-information-form');
     const confirmPasswordField = document.getElementById('input-confirmPassword');
     const errorSpan = document.getElementById('passwordError');
     let passwordMatch = true;
